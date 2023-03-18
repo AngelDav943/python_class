bfa2529ca234eb199d1594d515098c65242a6218
AngelDav943 <angel91.addcs@gmail.com>
2023-03-18 09:22:11 -0500

    [UPDATE] biblioapp

--- fast_api/biblioapp.py
+++ fast_api/biblioapp.py
@@ -2,39 +2,56 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import date
 
-app = FastAPI()
-
+app = FastAPI(
+    title="Biblioteca",
+    description="hola",
+    version="v0.0.1"
+)
 personas = []
-
-class Libro(BaseModel):
-    nombre:str
-    fecha:str
-
-class Persona(BaseModel):
-    nombre:str
-    libros:list[Libro]
 
 def persona_existe(id): # Verifica si usuario existe
     return id > 0 and id <= len(personas)
 
 @app.get("/")
-def hello_world():
+def root():
     return {
-        "titulo":"Biblioteca",
-        "version":"v0.0.1"
+        "titulo":app.title,
+        "version":app.version
     }
-    
-@app.get("/personas")
+
+@app.get("/personas", tags=["Personas"])
 def personas_all():
+    """Devuelve una lista de todas las personas en la biblioteca.
+
+    Returns:
+
+    - List: Lista de personas en biblioteca.
+    """
     return personas
 
-@app.get("/personas/{id}")
-def personas_all(id: int):
+@app.get("/personas/{id}", tags=["Personas"])
+def personas_buscar(id: int):
+    """Devuelve el usuario pedido.
+
+    Args:
+
+    - id (int): ID del usuario
+
+    Returns:
+
+    - dict: usuario.
+    """
     if persona_existe(id):
         return personas[id - 1]
-    
-@app.post("/personas")
+
+@app.post("/personas", tags=["Personas"])
 def personas_add(nombre: str):
+    """Agrega un usuario a los datos de la biblioteca.
+
+    Returns:
+
+    - dict: Devuelve el usuario.
+    """
     usuario = {
         "nombre": str(nombre).lower(),
         "libros": []
@@ -42,24 +59,74 @@ def personas_add(nombre: str):
     personas.append(usuario)
     return usuario
 
-@app.put("/personas")
+@app.put("/personas", tags=["Personas"])
 def personas_modify(id: int, nuevo_nombre:str):
+    """Modifica datos de un usuario existente.
+
+    Args:
+    - id (int): _description_
+    - nuevo_nombre (str): El nuevo nombre que le va a poner al usuario.
+
+    Returns:
+
+    - bool: Retorna True si el usuario existe.
+    """
     if persona_existe(id): # Verifica si usuario existe
         personas[id - 1]["nombre"] = str(nuevo_nombre).lower()
         return True
 
-@app.delete("/personas")
+@app.delete("/personas", tags=["Personas"])
 def personas_delete(id: int):
+    """Elimina un usuario a los datos de la biblioteca.
+
+    Args:
+
+    - id (int): Id del usuario.
+
+    Returns:
+
+    - bool: Devuelve True si el usuario existe.
+    """
     if persona_existe(id): # Verifica si usuario existe
         personas.pop(id - 1)
         return True
 
-@app.post("/libro")
+@app.post("/libro", tags=["Libros"])
 def libro_add(id: int, libro:str):
+    """Agrega un libro a un usuario.
+
+    Args:
+
+    - id (int): Id de la persona que va a prestar el libro.
+    - libro (str): Nombre del libro a prestar.
+
+    Returns:
+
+    - bool: Devuelve True si puede agregar el libro al usuario.
+    """
     if persona_existe(id):
         personas[id - 1]["libros"].append({
             "nombre": libro,
             "fecha": str(date.today()),
             "estado": "prestado"
         })
+        return True
+
+@app.delete("/libro", tags=["Libros"])
+def libro_remove(persona_id: int, libro_id:int):
+    """Elimina un libro de un usuario.
+
+    Args:
+
+    - persona_id (int): ID de la persona que va a eliminar el libro.
+    - libro_id (int): ID del libro a eliminar.
+
+    Returns:
+
+    - bool: Devuelve True si puede eliminar el libro del usuario.
+    """
+    if persona_existe(persona_id):
+        libros = personas[persona_id - 1]["libros"]
+        if libro_id > 0 and libro_id <= len(libros):
+            libros.pop(libro_id - 1)
         return True
---
+++ fast_api/sb.py
@@ -0,0 +1,17 @@
+var = {
+    1: {
+        "nombre":"Ang",
+        "libros":"Libro"
+    },
+    2: {
+        "nombre":"Ang",
+        "libros":"Libro"
+    }
+}
+
+var[4000]= {
+    "nombre":"Dav",
+    "libros":"libro"
+}
+print(var.get("nombre"))
+print(var[1])
